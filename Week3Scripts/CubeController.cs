89a393d4e6361bb3593077d85a618130455ec6db
NYCDOE <admin@AC1MM60JDF68C.local>
2016-04-02 12:29:42 -0400

    Uploaded Week3Scripts

---
+++ Week3Scripts/CubeController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+	public int Score;
+	// Update is called once per frame
+	void Update () {
+		float speed = 15 * Time.deltaTime; //Our speed in seconds
+
+		//Vector3 is a way to Pass 3D Directions or Positions. Very Useful
+		if (Input.GetKey (KeyCode.UpArrow)) {
+//			Debug.Log ("Up Arrow Pressed");
+			transform.Translate(Vector3.forward * speed);
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+//			Debug.Log ("Down Arrow Pressed");
+			transform.Translate(Vector3.back * speed);
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+//			Debug.Log ("Left Arrow Pressed");
+			transform.Translate(Vector3.left * speed);
+		}
+		if (Input.GetKey(KeyCode.RightArrow)) {
+//			Debug.Log ("Right Arrow Pressed");
+			transform.Translate(Vector3.right * speed);
+		}
+	}
+
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Coin") {
+			Score++;
+			Debug.Log (Score);
+			Destroy(other.gameObject);
+		}
+	}
+}
---
+++ Week3Scripts/startifStatements.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startifStatements : MonoBehaviour {
+
+	public bool fact = true;
+	// Use this for initialization
+	void Start () {
+		if (fact == true) {
+			fact = false;
+			if(fact == false) {
+				Debug.Log ("Fact was switched to False");
+			} else{
+				Debug.Log ("ERROR");
+			}
+		} else {
+			Debug.Log ("Condition wasn't met");
+		}
+
+//		if (10 < 20) {
+//			Debug.Log (3+4*5+1);
+//		}
+		//Multi If Statement
+//		if (fact == true) {
+//			Debug.Log (fact);
+//		}
+//		if (5 < 20) {
+//			fact = false;
+//			Debug.Log ("False");
+//		}
+		//Standard IfElse Statement
+//		if (10 < 20) {
+//			Debug.Log (3+4*5+1);
+//		}
+//		else {
+//			Debug.Log (1*8+(10-5));
+//		}
+	}
+	
+//	// Update is called once per frame
+//	void Update () {
+//	
+//	}
+}
